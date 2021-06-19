50d7fd1a708208ae0761ce9b21c5a716c7ba87e3
Ola Flisbäck <ola.flisback@gmail.com>
2021-06-19 10:55:56 +0200

    Print top commit info

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
+python -m venv env
 source env/bin/activate
 pip install -r requirements.txt
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import subprocess
+
 app = Flask(__name__)
+
+version = subprocess.check_output(["git", "reflog", "--date=iso"]).strip().decode().split('\n')[0]
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return version
